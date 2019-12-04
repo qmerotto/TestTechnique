62473e7ad4af4ae3570a27577b26d2d5410b95e6
qmerotto <qmerotto@aol.com>
2019-12-04 21:32:59 +0100

    delete part

--- src/main/java/http/HttpStatus.java
+++ src/main/java/http/HttpStatus.java
@@ -2,6 +2,8 @@ package http;
 
 public class HttpStatus {
     public static Integer OK = 200;
+    public static Integer CREATED = 201;
+    public static Integer ACCEPTED = 202;
     public static Integer BAD_REQUEST = 400;
     public static Integer NOT_FOUND = 404;
     public static Integer INTERNAL_ERROR = 404;
